e4c013478f811e76cd7e3ab84ea188aa2aad822c
ChrisDev <christian.lima999@gmail.com>
2022-02-22 22:15:14 -0300

    Versão com insert e select de imagens

---
+++ Verificacao por imagens/Form1.cs
@@ -0,0 +1,68 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Verificacao_por_imagens
+{   
+    public partial class Login : Form
+    {
+        Thread ligacao_paginas;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LinkCadastre_se_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Close();
+            ligacao_paginas = new Thread(gerencia_paginas.form_cadastro);
+            ligacao_paginas.SetApartmentState(ApartmentState.STA);
+            ligacao_paginas.Start();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        { 
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = openFileDialog1.FileName;
+                pictureBox1.Load();
+            }
+            // Rotina para puxar imagens do banco de dados:
+            //pictureBox1.Image = Image.FromStream((MemoryStream)query.select_imagens_mysql("imagem_nome","lilo-stitch-amigurumi-colecionavel.jpg"));
+        }
+
+        private void Entrar_Click(object sender, EventArgs e)
+        {
+            // Rotina pra verificação da imagem através do usuario apresentado
+            FileStream imagem = new FileStream(pictureBox1.ImageLocation, FileMode.Open, FileAccess.Read);
+            byte[] imagem_byte;
+            BinaryReader leitor_binario = new BinaryReader(imagem);
+            imagem_byte = leitor_binario.ReadBytes((int)imagem.Length);
+            var imagem_banco =(MemoryStream)query.select_imagens_mysql("usuario_login", usuario_textbox.Text);
+            if (imagem_byte.Length == imagem_banco.Length)
+            {
+                MessageBox.Show("é a mesma imagem!");
+            }
+        }
+    }
+    public class gerencia_paginas
+    {
+        public static void form_cadastro()
+        {
+            Application.Run(new Form_cadastro());
+        }
+        public static void form_login()
+        {
+            Application.Run(new Login());
+        }
+    }
+}
---
+++ Verificacao por imagens/conexao_bd.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace Verificacao_por_imagens
+{
+    class conexao_bd
+    {
+        // em casa:
+        // Banco de dados já com insercao!
+        // Não esquecer de trocar a string:
+        private static string str_conexao = "server=localhost;User Id=root;database=estudos;password=''";
+        public static MySqlConnection conexao = new MySqlConnection(str_conexao);
+    }
+    public class query
+    {
+        private static bool verifica_conexao()
+        {
+            bool res = false;
+            try
+            {
+                conexao_bd.conexao.Open();
+                res = true;
+            }
+            catch
+            {
+                res = false;
+                MessageBox.Show("Houve um erro na comunicação com a base de dados!");
+            }
+            return res;
+        }
+        public static object select_mysql( string campo_procurado,string valor_procurado,string tabela="imagem_em_bd")
+        {
+            bool res = false;
+            if (verifica_conexao())
+            {
+                // vou deixar estático a tabela de imagens:
+                string str_query = $"SELECT imagem FROM imagens_mysql WHERE {campo_procurado} = '{valor_procurado}'";
+                MySqlCommand select = new MySqlCommand(str_query, conexao_bd.conexao);
+                MySqlDataReader informacoes = select.ExecuteReader();
+                informacoes.Read();
+                if (informacoes[campo_procurado] != null)
+                {
+                    res = true;
+                }
+            }
+            return res;
+        }
+        public static object select_imagens_mysql(string campo_procurado, string valor_procurado, string tabela = "imagem_em_bd")
+        {
+            bool res = false;
+            if (verifica_conexao())
+            {
+                // vou deixar estático a tabela de imagens:
+                string str_query = $"SELECT imagem FROM imagens_mysql WHERE {campo_procurado} = '{valor_procurado}'";
+                MySqlCommand select = new MySqlCommand(str_query, conexao_bd.conexao);
+                MySqlDataReader informacoes = select.ExecuteReader();
+                informacoes.Read();
+                byte[] imagem = (byte[])informacoes["imagem"];
+                MemoryStream ms = new MemoryStream(imagem);
+                if (imagem.Length > 0)
+                {
+                    res = true;
+                    return ms;
+                }
+                conexao_bd.conexao.Close();
+            }
+            return res;
+        }
+        public static bool insert_imagem_mysql(object imagem)
+        {
+            bool res = false;
+            return res;
+        }
+    }
+}
